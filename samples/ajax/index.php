a496d17d616722d3a5a72942dc50d838bd284b39
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-08-14 19:35:21 +0000

    Adding in links to Spry samples.

---
+++ samples/ajax/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- DW6 -->
+<head>
+<!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<link rel="stylesheet" href="../../style.css" type="text/css" />
+</head>
+<!-- The structure of this file is exactly the same as 2col_leftNav.html;
+     the only difference between the two is the stylesheet they use -->
+<body>
+<div id="masthead">
+  <h1 id="siteName">(unofficial) Adobe PHP SDK</h1>
+  <div id="globalNav"> <a href="../samples/">Samples Main</a> | <a href="../samples/flex/">Flex Samples</a> | <a href="../samples/ajax/">Ajax Samples</a> | <a href="../samples/flex_ajax/">Flex / Ajax Samples</a></div>
+</div>
+<!-- end masthead -->
+<div id="content">
+  <h2 id="pageName">Welcome to the (unofficial) Adobe PHP SDK </h2>
+  <div class="feature">
+    <h3>The (unofficial) Adobe PHP SDK provides PHP developers with links and content to help them get started with Adobe Flex, Adobe's Spry Framework and PHP. </h3>
+  </div>
+  <div class="story">
+    <h3>Ajax Samples </h3>
+    <ol>
+      <li>Samples for the Spry Ajax framework can be found online at the Adobe Labs website at <a href="http://labs.adobe.com/technologies/spry/demos/ ">http://labs.adobe.com/technologies/spry/demos/</a>    </li>
+    </ol>
+  </div>
+</div>
+<!--end content -->
+<div id="navBar">
+  <h3>Related Link Category</h3>
+    <ul>
+      <li><a href="#">Adobe Flex PHP Developer Center</a></li>
+      <li><a href="#">Try Flex Builder</a></li>
+      <li><a href="#">Download Free Flex SDK</a></li>
+      <li><a href="#">AMFPHP</a></li>
+      <li><a href="#">Adobe Spry Ajax Framework</a></li>
+    </ul>
+  </div>
+</div>
+<!--end navBar div -->
+<div id="siteInfo"><!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a><br/>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5  License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/InteractiveResource" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by/2.5/"><permits rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> --></div>
+<br />
+</body>
+</html>
