27839588494d230aff84252c5a431752ef9b48f3
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-08-14 16:56:24 +0000

    Updating performance test with comments from Pete Mackie and Blain McDonnell.

---
+++ tests/performance/print_xml.php
@@ -0,0 +1,10 @@
+<?
+print "<rows>";
+
+for( $i = 0; $i < 40000; $i++ )
+{
+	print "<row><col1>This is row ".$i."</col1><col2>10000000</col2><col3>More text to add to this row.</col3></row>";
+}
+
+print "</rows>";
+?>
