c85a60e53d10c51021181836b476fa5bcded9c26
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-08-17 13:51:26 +0000

    Adding in an index page for the samples.

---
+++ samples/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- DW6 -->
+<head>
+<!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>(unofficial) Adobe PHP SDK</title>
+<link rel="stylesheet" href="../style.css" type="text/css" />
+</head>
+<!-- The structure of this file is exactly the same as 2col_leftNav.html;
+     the only difference between the two is the stylesheet they use -->
+<body>
+<div id="masthead">
+  <h1 id="siteName">(unofficial) Adobe PHP SDK Libraries </h1>
+  <div id="globalNav"> <a href="../samples">Samples Main</a> | <a href="flex">Flex Samples</a> | <a href="ajax">Ajax Samples</a> | <a href="flex_ajax">Flex / Ajax Samples</a></div>
+</div>
+<!-- end masthead -->
+<div id="content">
+  <h2 id="pageName">Welcome to the (unofficial) Adobe PHP SDK </h2>
+  <div class="story">
+    <h3>Getting Started</h3>
+    <ol>
+      <li>There are three directories of samples in this directory:
+        <ol>
+          <li>Ajax Samples - These samples make use of Adobe Ajax technology (Spry framework) and PHP.
+            <ol>
+              <li>Check out <a href="http://labs.adobe.com/technologies/spry/demos/">http://labs.adobe.com/technologies/spry/demos/</a> for great demos using PHP and the Spry framework. </li>
+            </ol>
+          </li>
+          <li>Flex Samples - These samples make use of Adobe Flex technology and PHP.
+            <ol>
+              <li><span class="code"><a href="flex/simple_amfphp/">simple_amfphp</a></span>/ - This sample shows the use of AMFPHP to retrieve a list of items from a MySQL database. Created by <a href="http://blogs.adobe.com/mikepotter/">Mike Potter</a>, updated by Pete Mackie. <br />
+                  <strong>Note: You must create the flextest database first. </strong>Use this <a href="flex/simple_amfphp/flextest.sql">flextest.sql</a> file to create the database and tables. </li>
+              <li><a href="flex/simple_flex_php/" class="code">simple_flex_php</a> - This sample shows the use of PHP to output XML, with a Flex front end to read that XML. Created by <a href="http://blogs.adobe.com/mikepotter/">Mike Potter</a>, updated by Pete Mackie. </li>
+              <li><a href="flex/simple_flex_xmlphp/" class="code">simple_flex_xmlphp</a> - This sample shows the use of Flex to read in XML created by a PHP script. The difference in this sample is that there's no database required. </li>
+            </ol>
+          </li>
+          <li>Flex / Ajax Samples - These samples make use of both Adobe Ajax technology (Spry framework) and Adobe Flex, with PHP on the backend.
+            <ol>
+              <li><a href="flex_ajax/feedburner/index.html">feedburner/</a> - This sample shows retreiving data from Feedburner via Ajax, then using that data to populate a graph made with Adobe Flex. Created by <a href="http://blogs.adobe.com/mikepotter/">Mike Potter</a>. </li>
+            </ol>
+          </li>
+        </ol>
+      </li>
+	</ol>
+  </div>
+</div>
+<!--end content -->
+<div id="navBar">
+  <h3>Related Links</h3>
+  </div>
+</div>
+<!--end navBar div -->
+<div id="siteInfo"><!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a><br/>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5  License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/InteractiveResource" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by/2.5/"><permits rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> --></div>
+<br />
+</body>
+</html>
