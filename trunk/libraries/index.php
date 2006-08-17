e084e5ab36c5454458ca201b82251685c3a65694
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-08-17 14:05:25 +0000

    Adding in index page for the libraries.

---
+++ trunk/libraries/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- DW6 -->
+<head>
+<!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>(unofficial) Adobe PHP SDK</title>
+<link rel="stylesheet" href="../style.css" type="text/css" />
+</head>
+<!-- The structure of this file is exactly the same as 2col_leftNav.html;
+     the only difference between the two is the stylesheet they use -->
+<body>
+<div id="masthead">
+  <h1 id="siteName">(unofficial) Adobe PHP SDK</h1>
+  <div id="globalNav"> <a href="../samples">Samples Main</a> | <a href="../samples/flex">Flex Samples</a> | <a href="../samples/ajax">Ajax Samples</a> | <a href="../samples/flex_ajax">Flex / Ajax Samples</a></div>
+</div>
+<!-- end masthead -->
+<div id="content">
+  <h2 id="pageName">(unofficial) Adobe PHP SDK Libraries </h2>
+  <div class="story">
+    <h3>Getting Started</h3>
+    <ol>
+      <li>There are 4 libraries that you need to run the samples.
+        <ol>
+          <li><a href="http://www.amfphp.org/">AMFPHP</a> - Download this and install it in the <span class="code">amfphp/</span> directory. Should be included with the .zip file. </li>
+          <li>Flex / Ajax Bridge - This is installed for you.</li>
+          <li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Framework</a> - Download this and install to the<span class="code"> spry/</span> directory. Should be included with the .zip file.</li>
+        </ol>
+      </li>
+    </ol>
+  </div>
+</div>
+<!--end content -->
+<div id="navBar">
+  <h3>Related Links</h3>
+  </div>
+</div>
+<!--end navBar div -->
+<div id="siteInfo"><!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a><br/>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5  License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	<Work rdf:about="">
+		<license rdf:resource="http://creativecommons.org/licenses/by/2.5/" />
+	<dc:type rdf:resource="http://purl.org/dc/dcmitype/InteractiveResource" />
+	</Work>
+	<License rdf:about="http://creativecommons.org/licenses/by/2.5/"><permits rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> --></div>
+<br />
+</body>
+</html>
