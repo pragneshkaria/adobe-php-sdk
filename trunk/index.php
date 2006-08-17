873653c69cf50857ced53df717c0185d9d776144
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-08-17 14:06:03 +0000

    Updating for reasons that I dont' remember right now.

--- trunk/index.php
+++ trunk/index.php
@@ -4,7 +4,7 @@
 <head>
 <!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
+<title>(unofficial) Adobe PHP SDK</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <!-- The structure of this file is exactly the same as 2col_leftNav.html;
@@ -12,7 +12,7 @@
 <body>
 <div id="masthead">
   <h1 id="siteName">(unofficial) Adobe PHP SDK</h1>
-  <div id="globalNav"> <a href="samples/">Samples Main</a> | <a href="samples/flex/">Flex Samples</a> | <a href="samples/ajax/">Ajax Samples</a> | <a href="samples/flex_ajax/">Flex / Ajax Samples</a></div>
+  <div id="globalNav"> <a href="samples">Samples Main</a> | <a href="samples/flex">Flex Samples</a> | <a href="samples/ajax">Ajax Samples</a> | <a href="samples/flex_ajax">Flex / Ajax Samples</a></div>
 </div>
 <!-- end masthead -->
 <div id="content">
@@ -23,33 +23,48 @@
   <div class="story">
     <h3>Getting Started</h3>
     <ol>
-      <li>Download the following packages and put them into the directories in this SDK.
+      <li>The following projects are included in this .zip file, and do not need to be downloaded:
         <ol>
-	<li><a href="http://www.amfphp.org">AMFPHP</a> - <a href="http://sourceforge.net/project/showfiles.php?group_id=72483#files">Download</a> this and install into <span class="code">libraries/amfphp/</span> directory.</li>
-	<li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework</a> - <a href="http://www.macromedia.com/go/labs_spry_download">Download</a> this and install into <span class="code">libraries/spry/</span> directory. <br />
-	  Note: You will likely have to move the files from a version specific directory( ie. <span class="code">libraries/Spry_1_2</span>) to the <span class="code">libraries/spry/</span> directory. </li>
-	<li><a href="http://framework.zend.com/">Zend Framework</a> - <a href="http://framework.zend.com/download">Download</a> this and install into <span class="code">libraries/zend/</span> directory. <br />
-	  Note: You will likely have to move the files from a version specific directory (ie. <span class="code">libraries/ZendFramework-0.1.5</span>) to the <span class="code">libraries/zend</span> directory. </li>
+	<li><a href="http://www.amfphp.org">AMFPHP (1.2.5)</a> - This is installed in the <span class="code">libraries/amfphp/</span> directory. The service browser is not included in this library. If you need that feature, please download AMFPHP and install it into <span class="code">libraries/amfphp/</span>. </li>
+	<li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework (1.3)</a> - This is installed in the <span class="code">libraries/spry/</span> directory. <br />
+	</li>
+	<li><a href="http://labs.adobe.com/wiki/index.php/Flex_Framework:FABridge">Flex / Ajax bridge</a> - This is installed in the <span class="code">libraries/fabridge/</span> directory. </li>
 	</ol>
     </li>
-	<li><a href="samples/">Browse the samples</a> in the <a href="samples/" class="code">samples/</a> directory.</li>
-    <li>Now, build some cool stuff with PHP, Flash and Ajax, and add it to our codebase. Currently, you can particiapte in our community in a number of ways:
+	  <!--<li>The following projects are not included in this .zip file, and may need to be dowloaded:
+	    <ol>
+	      <li><a href="http://framework.zend.com/">Zend Framework</a> - <a href="http://framework.zend.com/download">Download</a> this and install into <span class="code">libraries/zend/</span> directory. <br />
+Note: You will likely have to move the files from a version specific directory (ie. <span class="code">libraries/ZendFramework-0.1.5</span>) to the <span class="code">libraries/zend</span> directory.<br />
+	      Note: Currently there are no samples that use the Zend Framework. </li>
+        </ol>
+	  </li>-->
+	  <li><a href="samples">Browse the samples</a> in the <a href="samples" class="code">samples/</a> directory.</li>
+    </ol>
+	<h3>Getting Involved</h3>
+    <p>Now, build some cool stuff with PHP, Flash and Ajax, and add it to our codebase. Currently, you can particiapte in our community in a number of ways:
     <ol>
-      <li>Get on the <a href="http://groups.google.com/group/flex_php_sdk/">Google Groups mailing list</a>.</li>
-      <li>Update code, create samples in our <a href="http://code.google.com/p/flex-php-sdk/">project.</a> </li>
-      <li><a href="http://code.google.com/p/flex-php-sdk/issues/list">Add bugs</a> via the <a href="http://code.google.com/p/flex-php-sdk/issues/list">bug system</a>. </li>
-    </ol></ol>
+      <li>Get on the <a href="http://groups.google.com/group/adobe_php_sdk/">Adobe PHP SDK mailing list</a> at Google Groups.</li>
+      <li>Update code, create samples in our <a href="http://code.google.com/p/adobe-php-sdk/">project</a> hosted at Google Code. </li>
+      <li><a href="http://code.google.com/p/adobe-php-sdk/issues/entry">Submit bugs</a> via the <a href="http://code.google.com/p/adobe-php-sdk/issues/list">bug system</a>. </li>
+    </ol>
+    <h3>Learning More </h3>
+    <p>Want to learn how to integrate Flex or Ajax into your existing PHP applications? Read more about integrating Flex and PHP in the <a href="http://www.adobe.com/devnet/flex/flex_php.html">Flex PHP Developer Center</a>. Read more about integrating PHP and the Spry framework on the <a href="http://labs.adobe.com/technologies/spry/">Adobe Labs website</a>. </p>
+    <h3>Other Links</h3>
+    <ul>
+		<li><a href="http://renaun.com/blog/category/rich-internet-applications/amfphp/">Renaun.com AMFPHP blog section</a></li>
+		<li><a href="http://www.5etdemi.com/blog/archives/category/php/">Patrick Mineault's Blog - PHP section</a> - Patrick is one of the lead developers on AMFPHP. </li>
+	    <li><a href="http://www.themidnightcoders.com">The Midnight Coders</a> - WebORB for PHP coming soon. </li>
+    </ul>
   </div>
 </div>
 <!--end content -->
 <div id="navBar">
-  <h3>Related Link Category</h3>
+  <h3>Related Links</h3>
     <ul>
-      <li><a href="#">Adobe Flex PHP Developer Center</a></li>
-      <li><a href="#">Try Flex Builder</a></li>
-      <li><a href="#">Download Free Flex SDK</a></li>
-      <li><a href="#">AMFPHP</a></li>
-      <li><a href="#">Adobe Spry Ajax Framework</a></li>
+      <li><a href="http://www.adobe.com/devnet/flex/flex_php.html">Adobe Flex PHP Developer Center</a></li>
+      <li><a href="http://www.adobe.com/cfusion/tdrc/index.cfm?product=flex">Try Flex Builder</a></li>
+      <li><a href="http://www.adobe.com/cfusion/tdrc/index.cfm?product=flex">Download Free Flex SDK</a></li>
+      <li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework</a></li>
     </ul>
   </div>
 </div>
