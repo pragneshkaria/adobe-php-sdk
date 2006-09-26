56b8dea961099bbbee2c7c320c12c17021be083f
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-09-26 21:06:44 +0000

    New look for webpages in SDK.

--- index.php
+++ index.php
@@ -1,80 +1,123 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<!-- DW6 -->
+<html>
+
 <head>
-<!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>(unofficial) Adobe PHP SDK</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title></title>
+<link href="style.css" rel="stylesheet" type="text/css">
 </head>
-<!-- The structure of this file is exactly the same as 2col_leftNav.html;
-     the only difference between the two is the stylesheet they use -->
-<body>
-<div id="masthead">
-  <h1 id="siteName">(unofficial) Adobe PHP SDK</h1>
-  <div id="globalNav"> <a href="samples">Samples Main</a> | <a href="samples/flex">Flex Samples</a> | <a href="samples/ajax">Ajax Samples</a> | <a href="samples/flex_ajax">Flex / Ajax Samples</a></div>
-</div>
-<!-- end masthead -->
-<div id="content">
-  <h2 id="pageName">Welcome to the (unofficial) Adobe PHP SDK </h2>
-  <div class="feature">
-    <h3>The (unofficial) Adobe PHP SDK provides PHP developers with links and content to help them get started with Adobe Flex, Adobe's Spry Framework and PHP. </h3>
-  </div>
-  <div class="story">
-    <h3>Getting Started</h3>
-    <ol>
-      <li>The following projects are included in this .zip file, and do not need to be downloaded:
-        <ol>
-	<li><a href="http://www.amfphp.org">AMFPHP (1.2.5)</a> - This is installed in the <span class="code">libraries/amfphp/</span> directory. The service browser is not included in this library. If you need that feature, please download AMFPHP and install it into <span class="code">libraries/amfphp/</span>. </li>
-	<li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework (1.3)</a> - This is installed in the <span class="code">libraries/spry/</span> directory. <br />
-	</li>
-	<li><a href="http://labs.adobe.com/wiki/index.php/Flex_Framework:FABridge">Flex / Ajax bridge</a> - This is installed in the <span class="code">libraries/fabridge/</span> directory. </li>
-	</ol>
-    </li>
-	  <!--<li>The following projects are not included in this .zip file, and may need to be dowloaded:
-	    <ol>
-	      <li><a href="http://framework.zend.com/">Zend Framework</a> - <a href="http://framework.zend.com/download">Download</a> this and install into <span class="code">libraries/zend/</span> directory. <br />
-Note: You will likely have to move the files from a version specific directory (ie. <span class="code">libraries/ZendFramework-0.1.5</span>) to the <span class="code">libraries/zend</span> directory.<br />
-	      Note: Currently there are no samples that use the Zend Framework. </li>
-        </ol>
-	  </li>-->
-	  <li><a href="samples">Browse the samples</a> in the <a href="samples" class="code">samples/</a> directory.</li>
-    </ol>
-	<h3>Getting Involved</h3>
-    <p>Now, build some cool stuff with PHP, Flash and Ajax, and add it to our codebase. Currently, you can particiapte in our community in a number of ways:
-    <ol>
-      <li>Get on the <a href="http://groups.google.com/group/adobe_php_sdk/">Adobe PHP SDK mailing list</a> at Google Groups.</li>
+
+<body bgcolor="#F8F8F8" topmargin="0" marginheight="0">
+
+<div align="center">
+
+<table border="0" width="850" cellspacing="0" cellpadding="0">
+	<tr>
+		<td valign="top" colspan="3" width="850" align="center">
+		<img border="0" src="images/hdr01.gif" width="850" height="42"><br>
+		<a href="index.html">
+		<img border="0" src="images/hdr06.gif"></a><img border="0" src="images/hdr03.jpg" width="189" height="110"><img border="0" src="images/hdr04.gif" width="59" height="110"></td>
+	</tr>
+	<tr>
+		<td valign="top" bgcolor="#CCCCCC" width="1"><img border="0" src="images/spacer.gif" width="1" height="100%"></td>
+		<td valign="top" width="848" bgcolor="#FFFFFF" align="center">
+		<div id="Nav">
+		<img border="0" src="images/hdr05.gif" width="848" height="23"><br>
+		<a href="samples/">SAMPLES MAIN</a>&nbsp; |&nbsp; <a href="samples/flex/">FLEX SAMPLES</a>&nbsp; |&nbsp; <a href="samples/ajax/">AJAX SAMPLES</a>&nbsp; |&nbsp; <a href="samples/flex_ajax/">FLEX / AJAX SAMPLES</a><br>
+		</div>
+		<img border="0" src="images/line06.gif" width="848" height="3">
+		
+		
+		
+		
+		
+		<table border="0" width="848" cellspacing="0" cellpadding="15">
+	<tr>
+		<td valign="top" width="568" class="copy"><span class="header01">Welcome to the PHP RIA SDK by Adobe </span><br>
+		The PHP RIA SDK by Adobe provides PHP developers with links and 
+		content to help you get started with Adobe Flex, Adobe's Spry Framework 
+		and PHP.
+		  <p><span class="header01">Getting Started</span><ol>
+			<li><a href="http://www.adobe.com/go/flex_trial">Download the free Adobe Flex SDK</a> or the <a href="http://www.adobe.com/go/flex_trial">free trial of Adobe Flex Builder</a>.</li> 
+			<br />            
+			<li>Get PHP from <a href="http://www.php.net/">PHP.net</a>.</li><br />
+			<li>Learn the basics:
+			<ul>
+			  <li><a href="http://www.adobe.com/devnet/flex/articles/flex2_php.html">Integrating Flex and PHP</a></li>
+			  <li><a href="http://www.adobe.com/devnet/flex/articles/flex2_amfphp.html">Using Flex and AMFPHP</a></li>
+			</ul> 
+			<br />                        
+			<li>The following projects are included in this .zip file, and do not need 
+			  to be downloaded:<br>
+			  <ol>
+			    <li><a href="http://www.amfphp.org">AMFPHP (1.2.5)</a> - This is installed in the <span class="code">libraries/amfphp/</span> directory. The service browser is not included in this library. If you need that feature, please download AMFPHP and install it into <span class="code">libraries/amfphp/</span>. </li>
+	    <li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework (1.3)</a> - This is installed in the <span class="code">libraries/spry/</span> directory. <br />
+	      </li>
+	    <li><a href="http://labs.adobe.com/wiki/index.php/Flex_Framework:FABridge">Flex / Ajax bridge</a> - This is installed in the <span class="code">libraries/fabridge/</span> directory. </li>
+		    </ol><br />
+			  <li>The following projects are not included in this .zip file, and <em>may</em> need to be downloaded in order to run certain samples:</li>
+			<ol>
+			  <li><a href="http://themidnightcoders.com/weborb/php/index.htm">WebORB for PHP</a> - <a href="http://themidnightcoders.com/weborb/php/weborb-php-latest.zip">Download it</a> from the Midnight Coders and <a href="http://themidnightcoders.com/weborb/php/gettingstarted.htm">install it as per these instructions</a>. </li>
+			</ol><br />
+			<li>Browse the samples in the <a class="code" href="samples/">samples/</a> directory.</li>
+			</ol>
+			<p>
+		
+		
+		<span class="header01">Getting Involved</span><br>
+		Now, build some cool stuff with PHP, Flash and Ajax, and add it to our 
+		codebase. Currently, you can particiapte in our community in a number of 
+		ways:<ol>
+			<li>Get on the <a href="http://groups.google.com/group/adobe_php_sdk/">Adobe PHP SDK mailing list</a> at Google Groups.</li>
       <li>Update code, create samples in our <a href="http://code.google.com/p/adobe-php-sdk/">project</a> hosted at Google Code. </li>
       <li><a href="http://code.google.com/p/adobe-php-sdk/issues/entry">Submit bugs</a> via the <a href="http://code.google.com/p/adobe-php-sdk/issues/list">bug system</a>. </li>
-    </ol>
-    <h3>Learning More </h3>
-    <p>Want to learn how to integrate Flex or Ajax into your existing PHP applications? Read more about integrating Flex and PHP in the <a href="http://www.adobe.com/devnet/flex/flex_php.html">Flex PHP Developer Center</a>. Read more about integrating PHP and the Spry framework on the <a href="http://labs.adobe.com/technologies/spry/">Adobe Labs website</a>. </p>
-    <h3>Other Links</h3>
-    <ul>
-		<li><a href="http://renaun.com/blog/category/rich-internet-applications/amfphp/">Renaun.com AMFPHP blog section</a></li>
-		<li><a href="http://www.5etdemi.com/blog/archives/category/php/">Patrick Mineault's Blog - PHP section</a> - Patrick is one of the lead developers on AMFPHP. </li>
-	    <li><a href="http://www.themidnightcoders.com">The Midnight Coders</a> - WebORB for PHP coming soon. </li>
-    </ul>
-  </div>
-</div>
-<!--end content -->
-<div id="navBar">
-  <h3>Related Links</h3>
-    <ul>
-      <li><a href="http://www.adobe.com/devnet/flex/flex_php.html">Adobe Flex PHP Developer Center</a></li>
-      <li><a href="http://www.adobe.com/cfusion/tdrc/index.cfm?product=flex">Try Flex Builder</a></li>
-      <li><a href="http://www.adobe.com/cfusion/tdrc/index.cfm?product=flex">Download Free Flex SDK</a></li>
-      <li><a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework</a></li>
-    </ul>
-  </div>
-</div>
-<!--end navBar div -->
-<div id="siteInfo"><!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a><br/>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5  License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+			</ol>
+			<p>
+		
+		
+		<span class="header01">Learning More</span><br>
+		Want to learn how to integrate Flex or Ajax into your existing PHP 
+		applications?             
+			<p>Read more about integrating Flex and PHP in the <a href="http://www.adobe.com/devnet/flex/flex_php.html">Flex PHP Developer Center</a>.             
+			<p>Read more about integrating PHP and the Spry framework 
+			  on the <a href="http://labs.adobe.com/technologies/spry/">Adobe Labs website</a>.             
+			<p>Read the <a href="http://livedocs.macromedia.com/flex/2/langref/">Flex Documentation</a>.</td>
+		
+		
+		
+		<td valign="top" width="1" height="100%"><img border="0" src="images/divi01.gif" width="1" height="100%"></td>
+		
+		
+		
+		<td valign="top" width="220" class="copy"><span class="header01">Related Links</span><br>
+		� <a href="http://www.adobe.com/devnet/flex/">Adobe Flex PHP Developer Center</a><br>
+		� <a href="http://www.adobe.com/go/flex_trial">Try Flex Builder</a><br>
+		� <a href="http://www.adobe.com/go/flex_trial">Download Free Flex SDK</a><br>
+		� <a href="http://labs.adobe.com/technologies/spry/">Adobe Spry Ajax Framework</a></td>
+	</tr>
+</table>
+
+		</td>
+		<td valign="top" bgcolor="#CCCCCC" width="1"><img border="0" src="images/spacer.gif" width="1" height="100%"></td>
+	</tr>
+	<tr>
+		<td valign="top" colspan="3" width="850">
+		<img border="0" src="images/ftr01.gif" width="850" height="20"><br>
+		<img border="0" src="images/ftr02.gif" width="850" height="24">
+		<p class="copyright"><!--Creative Commons License--><a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img alt="Creative Commons License" border="0" src="http://creativecommons.org/images/public/somerights20.png"/></a><br/>
+		  The webpages are  licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5  License</a>.<!--/Creative Commons License--><!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
 	<Work rdf:about="">
 		<license rdf:resource="http://creativecommons.org/licenses/by/2.5/" />
 	<dc:type rdf:resource="http://purl.org/dc/dcmitype/InteractiveResource" />
 	</Work>
-	<License rdf:about="http://creativecommons.org/licenses/by/2.5/"><permits rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> --></div>
-<br />
+	<License rdf:about="http://creativecommons.org/licenses/by/2.5/"><permits rdf:resource="http://web.resource.org/cc/Reproduction"/><permits rdf:resource="http://web.resource.org/cc/Distribution"/><requires rdf:resource="http://web.resource.org/cc/Notice"/><requires rdf:resource="http://web.resource.org/cc/Attribution"/><permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/></License></rdf:RDF> --> 
+		  <br />
+		  This code in the SDK is made available in an <a href="LICENSE.TXT">open source license</a>. The code under the <span class="code">libraries/</span> directory maintains its original license. 
+		</td>
+	</tr>
+</table>
+
+	</div>
+
 </body>
+
 </html>
