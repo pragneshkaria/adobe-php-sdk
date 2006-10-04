36666df00d61a1d0bf64c3b0cfd5b06e87990dc2
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-10-04 18:40:12 +0000

    Adding in weborb services and directory.

---
+++ trunk/libraries/weborb/Services/dbio_example.php
@@ -0,0 +1,82 @@
+<?php
+Define('DATABASE_SERVER', 'localhost');
+Define('DATABASE_USERNAME', 'flext');
+Define('DATABASE_PASSWORD', 'p@ssword');
+Define('DATABASE_NAME', 'flextest');
+
+// MySQL database I/O PHP class example as a WebORB for PHP service
+class dbio_example {
+	private $mysqli;
+	private $err_prefix="WebORB Remoting Error: 'dbio_example' class database ";
+
+	public function __construct() {
+		error_reporting(0);	# Silence error messages and return them to WebORB
+		# Connect to MySQL database....
+		$this->mysqli = new mysqli(DATABASE_SERVER, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);         
+		if (mysqli_connect_errno()) {
+			$msg=$this->err_prefix."could not connect: ".mysqli_connect_error();
+   		throw new Exception($msg);
+   	}
+	}
+
+	public function getUsers() {
+		# Return a list of all the 'flextest' users
+		if (!$result=$this->mysqli->query("SELECT * from users")) {
+			$msg=$this->err_prefix."SELECT query error: ".$this->mysqli->error;
+			$this->mysqli->close();
+   		throw new Exception($msg);
+		}
+		while ($row = $result->fetch_assoc()) {
+			$user_array[] = $row;
+		}
+		return($user_array);
+	}
+
+	public function submitUser(array $user) {
+		# Escape special characters 
+		$name=$this->mysqli->real_escape_string(trim($user['username']));
+		$addr=$this->mysqli->real_escape_string(trim($user['emailaddress']));
+		$query = "INSERT INTO users VALUES ('', '$name', '$addr')";
+		if (!$result=$this->mysqli->query($query)) {
+			$msg=$this->err_prefix."INSERT query error: ".$this->mysqli->error;
+			$this->mysqli->close();
+   		throw new Exception($msg);
+		}
+	}
+
+	public function deleteUser($userid) {
+		$query = "DELETE FROM users WHERE userid='$userid'";
+		if (!$result=$this->mysqli->query($query)) {
+			$msg=$this->err_prefix."DELETE query error: ".$this->mysqli->error;
+			$this->mysqli->close();
+   		throw new Exception($msg);
+		}
+	}
+
+	// --- PHP Exception examples for testing and evaluation of Flex 2 Exception Handling ---
+	// --- NEVER PART OF A REAL SOLUTION ---
+
+	public function submitUserException(array $user) {
+		# Escape special characters 
+		$name=$this->mysqli->real_escape_string(trim($user['username']));
+		$addr=$this->mysqli->real_escape_string(trim($user['emailaddress']));
+		$query = "INSERT INTO xxxusers VALUES ('', '$name', '$addr')";
+		if (!$result=$this->mysqli->query($query)) {
+			$msg=$this->err_prefix."INSERT query error: ".$this->mysqli->error;
+			$this->mysqli->close();
+   		throw new Exception($msg);
+		}
+	}
+	
+	public function deleteUserException($userid) {
+		$query = "DELETE FROM usersxxxx WHERE userid='$userid'";
+		if (!$result=$this->mysqli->query($query)) {
+			$msg=$this->err_prefix."DELETE query error: ".$this->mysqli->error;
+			$this->mysqli->close();
+   		throw new Exception($msg);
+		}
+	}
+
+}
+
+?>
