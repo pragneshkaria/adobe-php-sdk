e9a945d62bd565b27be6eecd84b64eabee6c51f3
mike.potter.adobe <mike.potter.adobe@2cc0c2d0-321a-0410-8403-2fdd6aa20dfb>
2006-11-07 19:21:48 +0000

    Adding new samples to the HTML pages.

--- trunk/samples/flex/simple_flex_xmlphp/print_xml.php
+++ trunk/samples/flex/simple_flex_xmlphp/print_xml.php
@@ -5,11 +5,12 @@ $start = microtime();
 
 for( $i = 0; $i < $_GET["rows"]; $i++ )
 {
-	print "<row><col1>This is row ".$i."</col1>".
-		"<col2>";
-	print microtime() - $start;
-	print "</col2>".
-		"<col3>More text to add to this row.</col3></row>";
+	print "<row>\n\t<col1>This is row ".$i."</col1>".
+		"\n\t<col2>";
+	$time = microtime() - $start;
+	$time *= 1000;
+	print $time."</col2>".
+		"\n\t<col3>More text to add to this row.</col3></row>\n";
 }
 
 print "</rows>";
